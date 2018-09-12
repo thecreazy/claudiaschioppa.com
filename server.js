9af6e468e7355b13e7cdc7105c81ee72b0e88921
Riccardo Canella <canella_riccardo@icloud.com>
2018-09-12 19:49:13 +0200

    using the NODE_PORT

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3000, err => {
+  server.listen(process.env.NODE_PORT || 3000, err => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
   });
